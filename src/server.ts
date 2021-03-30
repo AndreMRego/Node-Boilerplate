9bb07ee45b0b05df1c4ef3642a85194bab7ceb8e
Andre Rego <andremregodev@gmail.com>
2021-03-29 22:47:53 -0300

    add folder with routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 
+import { router } from "./routes";
+
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Working....'});
-});
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
