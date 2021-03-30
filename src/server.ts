a9cdfbf6c3ad4cf74852f27935f7166d2390d112
Andre Rego <andremregodev@gmail.com>
2021-03-29 22:41:43 -0300

    add ts-node-env to execute typescript em dev mode

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (request, response) => {
-  return response.json({ message: 'Working...'});
+  return response.json({ message: 'Working....'});
 });
 
 app.listen(3333, () => console.log("Server is running!"));
