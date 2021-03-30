5854c6a06a30e87d5b4a74f97331f2cd679685d9
Andre Rego <andremregodev@gmail.com>
2021-03-30 19:22:53 -0300

    create migration of games

---
+++ src/database/migrations/1617073144906-CreateGames.ts
@@ -0,0 +1,35 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateGames1617073144906 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'games',
+            columns: [
+                {
+                    name: "id",
+                    type: "uuid",
+                    isPrimary: true
+                },
+                {
+                    name: "name",
+                    type: "varchar",
+                },
+                {
+                    name: "description",
+                    type: "varchar",
+                },
+                {
+                    name: "created_at",
+                    type: "timestamp",
+                    default: "now()"
+                }
+            ]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('games')
+    }
+
+}
